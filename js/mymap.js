843bd5665a56c91422b0f7f0d033156b686c7111
Stefanny Aguirre <sbaguirr@espol.edu.ec>
2019-06-20 02:04:00 -0500

    Geolocalización en vista contacto

---
+++ js/mymap.js
@@ -0,0 +1,9 @@
+var mymap= L.map('mapid').setView([-2.144446, -79.967730],10);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 25,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYWF2ZW5kYW4iLCJhIjoiY2p3NnVzdHozMjdxeDQzcXBnYjlwMTRqcyJ9.S00xReWyD9_Eb4B1h-VgIg'
+}).addTo(mymap);
+
+var marker = L.marker([-2.144446, -79.967730]).addTo(mymap);
