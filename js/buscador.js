00fa025f63964162fe8c9340de19854618dfb54d
Stefanny Aguirre <sbaguirr@espol.edu.ec>
2019-07-12 01:53:41 -0500

    Implementación de buscador en páginas principales

---
+++ js/buscador.js
@@ -0,0 +1,10 @@
+$("#inputBuscar").keyup(function () {
+  var consulta = $('#searchTabla').DataTable();
+  consulta.search($(this).val()).draw();
+	if ($("#inputBuscar").val()==""){
+		$("#busqueda").hide();
+
+	} else {
+		$("#busqueda").fadeIn("fast");
+  }
+});
